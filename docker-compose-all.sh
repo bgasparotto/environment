70403dfb56ae05c044debdff5c8679b68d0fcc75
bgasparotto <bruno.m.gasparotto@gmail.com>
2020-12-20 16:19:47 +0100

    Fixed an error where the docker-compose files were not being found when this sh was called by an external script.

--- docker-compose-all.sh
+++ docker-compose-all.sh
@@ -1,3 +1,6 @@
-docker-compose -f hdfs/docker-compose.yml "$@"
-docker-compose -f kafka/docker-compose.yml "$@"
-docker-compose -f spark/docker-compose.yml "$@"
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+docker-compose -f "$DIR"/hdfs/docker-compose.yml "$@"
+docker-compose -f "$DIR"/kafka/docker-compose.yml "$@"
+docker-compose -f "$DIR"/spark/docker-compose.yml "$@"
